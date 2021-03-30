c1b1625d2145e553f03e771bd9c9fd7b61cb6ac0
Jesse Brown <Brownjesse8895@yahoo.com>
2021-03-30 18:22:08 -0500

    commit lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+  const employee = {
+  name: "Jesse",
+    streetAddress: "2001 Faro Dr Austin, TX 78741"
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, { [key]: value })
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value
+    return employee 
+}
+
+function deleteFromEmployeeByKey(employee, key){
+  const newObject = Object.assign({}, employee)
+    delete newObject[key]
+    return newObject
+} 
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key]
+    return employee
+    
+}
+
