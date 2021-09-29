093dfb797bed35fc788efdb6c8eecb20c020b733
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-29 17:24:50 -0500

    commit tree 7bafca872d160b50bf349ea446ece90fb24af800

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const updateEmployeeWithKeyAndValue = (employee, key, value) => {
   return {
     ...employee, [key]: value
   }
+    
 }
 const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
     employee[key] = value 
@@ -54,30 +55,4 @@ const destructivelyDeleteFromEmployeeByKey = (employee, key ) => {
 
 
 
-
-// // Write your solution in this file!
-//   const employee = {
-//   name: "Jesse",
-//     streetAddress: "2001 Faro Dr Austin, TX 78741"
-// }
-// function updateEmployeeWithKeyAndValue(employee, key, value){
-//     return Object.assign({}, employee, { [key]: value })
-// }
-
-// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-//     employee[key] = value
-//     return employee 
-// }
-
-// function deleteFromEmployeeByKey(employee, key){
-//   const newObject = Object.assign({}, employee)
-//     delete newObject[key]
-//     return newObject
-// } 
-
-// function destructivelyDeleteFromEmployeeByKey(employee, key){
-//     delete employee[key]
-//     return employee
-    
-// }
 
