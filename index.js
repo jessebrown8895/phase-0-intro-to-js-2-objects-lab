f04ef660d7e0252b8a992a3fef18c40613c4e2e2
Jesse Brown <Brownjesse8895@yahoo.com>
2021-09-29 09:26:06 -0500

    Objects Lab
    In this lab we created aa variable of employee and assigned it the value of an object with with atwo keys name and streetAdress which have the values of sam and an address. I then created four arrow functions. The first one creates a copy of the employee object and updates the key value pair without modifying the origional. tThe second function destructively updates the employee key value pairs. The thrid object creates a copy of the origional oand delets a key and returns the the updated copy of the object without modifying the origional. The last function destructively deletes a key and returns the updated object

--- index.js
+++ index.js
@@ -1,26 +1,83 @@
-// Write your solution in this file!
-  const employee = {
-  name: "Jesse",
-    streetAddress: "2001 Faro Dr Austin, TX 78741"
-}
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    return Object.assign({}, employee, { [key]: value })
+const employee = {
+  name: "Sam",
+  streetAddress: "2001 Faro DR unit 6 Austin TX 78741"
 }
 
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
-    employee[key] = value
-    return employee 
+const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+  return {
+    ...employee, [key]: value
+  }
 }
-
-function deleteFromEmployeeByKey(employee, key){
-  const newObject = Object.assign({}, employee)
-    delete newObject[key]
-    return newObject
-} 
-
-function destructivelyDeleteFromEmployeeByKey(employee, key){
-    delete employee[key]
+const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value 
     return employee
-    
 }
+
+const deleteFromEmployeeByKey = (employee, key) => {
+  let nondestructivelyDeleteKey = {...employee}
+  delete nondestructivelyDeleteKey[key]
+  return nondestructivelyDeleteKey
+}
+
+const destructivelyDeleteFromEmployeeByKey = (employee, key ) => {
+  delete employee[key]
+  return employee
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write your solution in this file!
+//   const employee = {
+//   name: "Jesse",
+//     streetAddress: "2001 Faro Dr Austin, TX 78741"
+// }
+// function updateEmployeeWithKeyAndValue(employee, key, value){
+//     return Object.assign({}, employee, { [key]: value })
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+//     employee[key] = value
+//     return employee 
+// }
+
+// function deleteFromEmployeeByKey(employee, key){
+//   const newObject = Object.assign({}, employee)
+//     delete newObject[key]
+//     return newObject
+// } 
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key){
+//     delete employee[key]
+//     return employee
+    
+// }
 
